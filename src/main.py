83fe08aa3866127f86dfa0f03e291f48c1b59211
iamgp21 <gaupande21@gmail.com>
2025-02-05 13:07:19 +0530

    Added Cloud Function source code & Github Action Workflow

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import base64
+import os
+import github
+from github import Github
+from github import Auth 
+
+patToken = os.environ.get("GITHUB_PAT")
+
+def trigger(event, context):
+    
+    try:
+
+        pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+        
+        # Auth using an access token:
+        auth = Auth.Token(str(patToken))
+        
+        gobj = Github(auth=auth)
+
+        user = gobj.get_user()
+        
+        ##Create a Github Repo:
+        repo = user.create_repo("testrepo")
+        
+        gobj.close()
+    
+    except Exception as err:
+
+        print("Caught exception: " + str(err))
+        
+        gobj.close()
+        
+def main():
+    trigger(event, context)
